e02841a62a51d9ebe5a32cc461f8b5ef72a42c4e
Ayush <ayushkathariyua@ggmail.com>
2023-10-09 14:08:31 +0545

    getPostById and likePostById added

--- controllers/postController.js
+++ controllers/postController.js
@@ -33,6 +33,55 @@ const createPostController = async (req, res) => {
   }
 };
 
+const getPostByIdController = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    if (!postId) {
+      return res.status(400).json({ message: "Post id is required" });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    return res.status(200).json({ post });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const likePostByIdController = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    if (!postId) {
+      return res.status(400).json({ message: "Post id is required" });
+    }
+
+    const curUserId = req._id;
+
+    const post = await Post.findById(postId).populate("user");
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    if (post.likes.includes(curUserId)) {
+      const index = post.likes.indexOf(curUserId);
+      post.likes.splice(index, 1);
+      await post.save();
+      return res.status(200).json({ message: "Post unliked successful" });
+    } else {
+      post.likes.push(curUserId);
+      await post.save();
+      return res.status(200).json({ message: "Post liked successful" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   createPostController,
+  getPostByIdController,
+  likePostByIdController,
 };
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const requireUser = require("../middlewares/requireUser");
-const { createPostController } = require("../controllers/postController");
+const {
+  createPostController,
+  getPostByIdController,
+  likePostByIdController,
+} = require("../controllers/postController");
 
 router.post("/", requireUser, createPostController);
+router.get("/:postId", requireUser, getPostByIdController);
+router.post("/:postId/like", requireUser, likePostByIdController);
 
 module.exports = router;
